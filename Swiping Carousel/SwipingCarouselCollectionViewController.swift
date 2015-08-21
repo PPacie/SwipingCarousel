38d242acce8ca350c396bd61d5002280164c34d6
Pablo Paciello <ppacie@gmail.com>
2015-08-21 00:51:07 -0300

    Add Colors Extension and set the cell.backgroundColor.

--- Swiping Carousel/SwipingCarouselCollectionViewController.swift
+++ Swiping Carousel/SwipingCarouselCollectionViewController.swift
@@ -19,6 +19,8 @@ class SwipingCarouselCollectionViewController: UICollectionViewController {
             collectionView?.reloadData()
         }
     }
+    
+    let colors = UIColor.palette()
         
     // MARK: UICollectionViewDataSource
 
@@ -38,9 +40,36 @@ class SwipingCarouselCollectionViewController: UICollectionViewController {
         cell.professionLabel.text = currentCard.profession
         cell.mainDescriptionLabel.text = currentCard.mainDescription
         cell.activityLabel.text = currentCard.activity
-        
+        cell.backgroundColor = colors[indexPath.item]
         return cell
     }
  
 
+}
+    //Extension to create a pallete of colors which is being used to set the cell.backgroundColor
+private extension UIColor {
+    
+    class func colorFromRGB(r: Int, g: Int, b: Int) -> UIColor {
+        return UIColor(red: CGFloat(Float(r) / 255), green: CGFloat(Float(g) / 255), blue: CGFloat(Float(b) / 255), alpha: 1)
+    }
+    
+    class func palette() -> [UIColor] {
+        let palette = [
+            UIColor.colorFromRGB(85, g: 0, b: 255),
+            UIColor.colorFromRGB(170, g: 0, b: 170),
+            UIColor.colorFromRGB(85, g: 170, b: 85),
+            UIColor.colorFromRGB(0, g: 85, b: 0),
+            UIColor.colorFromRGB(255, g: 170, b: 0),
+            UIColor.colorFromRGB(255, g: 85, b: 0),
+            UIColor.colorFromRGB(0, g: 85, b: 85),
+            UIColor.colorFromRGB(0, g: 85, b: 255),
+            UIColor.colorFromRGB(170, g: 170, b: 255),
+            UIColor.colorFromRGB(85, g: 0, b: 0),
+            UIColor.colorFromRGB(170, g: 85, b: 85),
+            UIColor.colorFromRGB(170, g: 255, b: 0),
+            UIColor.colorFromRGB(85, g: 170, b: 255),
+            UIColor.colorFromRGB(0, g: 170, b: 170)
+        ]
+        return palette
+    }
 }
