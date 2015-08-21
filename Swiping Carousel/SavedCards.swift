9ee3a5228e3c075f9c920d7a79433d600d87b7ad
Pablo Paciello <ppacie@gmail.com>
2015-08-21 00:10:07 -0300

    Add the model as a separated class. Organize the Folders =)

---
+++ Swiping Carousel/SavedCards.swift
@@ -0,0 +1,33 @@
+//
+//  SavedCards.swift
+//  Swiping Carousel
+//
+//  Created by Pablo Surfate on 8/20/15.
+//  Copyright (c) 2015 Pablo Paciello. All rights reserved.
+//
+
+import UIKit
+
+class SavedCards {
+    
+    struct Card {
+        let image: UIImage?
+        let name: String
+        let profession: String
+        let mainDescription: String
+        let activity: String
+    }
+    
+    //Load some demo information into the [savedCards] Array.
+    class func loadCards() -> [Card] {
+        var savedCards = [Card]()
+        for index in 1...10 {
+            let newCard = Card(image: UIImage(named:"image\(index).png"), name: "User\(index)", profession: "Profesion\(index)", mainDescription: "To share my idea with somebody! I want to start my own fashion company. It's going to be called Black Milk! Ping me if you want to chat 😜", activity: "Activity\(index)")
+            savedCards.append(newCard)
+        }
+        return savedCards
+    }
+
+    
+}
+
--- Swiping Carousel/SwipingCarouselCollectionViewController.swift
+++ Swiping Carousel/SwipingCarouselCollectionViewController.swift
@@ -12,59 +12,19 @@ let reuseIdentifier = "Card"
 
 class SwipingCarouselCollectionViewController: UICollectionViewController {
     
-    //MARK: Model
-    private var savedCards = [Card?]()
-    
-    private struct Card {
-        let image: UIImage?
-        let name: String
-        let profession: String
-        let mainDescription: String
-        let activity: String
-    }
-   
-    //Load some demo information into the [savedCards] Array.
-    private func loadCards() {
-        for index in 1...10 {
-            let newCard = Card(image: UIImage(named:"image\(index).png"), name: "User\(index)", profession: "Profesion\(index)", mainDescription: "To share my idea with somebody! I want to start my own fashion company. It's going to be called Black Milk! Ping me if you want to chat 😜", activity: "Activity\(index)")
-            savedCards.append(newCard)
+    // MARK: Model
+    // Load allTheCards from SavedCards Class and reload the collection view when setted.
+    private var allTheCards = SavedCards.loadCards() {
+        didSet {
+            collectionView?.reloadData()
         }
-        collectionView?.reloadData()
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        loadCards()
         
-        // Do any additional setup after loading the view.
-//        
-//        let pinchGesture = UIPinchGestureRecognizer(target: self, action:"handleGesture:")
-//        collectionView?.addGestureRecognizer(pinchGesture)
-    }
-    
-//    func handleGesture(gesture: UIPinchGestureRecognizer) {
-//        switch gesture.state {
-//        case .Began:
-//            let scaleStart = gesture.scale
-//        case .Changed:
-//            
-//            collectionView?.collectionViewLayout.invalidateLayout()
-//        default: break
-//        }
-//    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: UICollectionViewDataSource
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //#warning Incomplete method implementation -- Return the number of items in the section
-        return savedCards.count
+        //Return the number of items in the section
+        return allTheCards.count
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -72,19 +32,15 @@ class SwipingCarouselCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CardCollectionViewCell
         
         // Configure the cell
-        if let currentCard = savedCards[indexPath.row] {
-            cell.imageView.image = currentCard.image
-            cell.nameLabel.text = currentCard.name
-            cell.professionLabel.text = currentCard.profession
-            cell.mainDescriptionLabel.text = currentCard.mainDescription
-            cell.activityLabel.text = currentCard.activity
-        }
+        let currentCard = allTheCards[indexPath.row]
+        cell.imageView.image = currentCard.image
+        cell.nameLabel.text = currentCard.name
+        cell.professionLabel.text = currentCard.profession
+        cell.mainDescriptionLabel.text = currentCard.mainDescription
+        cell.activityLabel.text = currentCard.activity
+        
         return cell
     }
-
-
-    // MARK: UICollectionViewDelegate
-
  
 
 }
