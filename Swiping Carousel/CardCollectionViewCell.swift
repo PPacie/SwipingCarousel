1754feafbc74d97f1fa53993127ca3e07c7b1f16
Pablo Paciello <ppacie@gmail.com>
2015-08-22 22:32:36 -0300

    Improve the CardCollectionViewCell. Add the gesture recognizer in the init() method of the Cell.

--- Swiping Carousel/CardCollectionViewCell.swift
+++ Swiping Carousel/CardCollectionViewCell.swift
@@ -17,46 +17,37 @@ class CardCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var mainDescriptionLabel: UILabel!
     @IBOutlet weak var activityLabel: UILabel!
     
-//    required init(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//    }
-//    
-//    init(frame: CGRect, delegate: AnyObject) {
-//        super.init(frame: frame)
-//        
-//        
-//        // Initialization code
-//        //  self.delegate                 = (delegate as ProfileSelectorViewController)
-//        self.layer.cornerRadius = 10;
-//        self.layer.shadowRadius = 4;
-//        self.layer.shadowOpacity = 0.6;
-//        self.layer.shadowOffset = CGSizeMake(3, 3);
-//
-// 
-//    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        // ...
+        self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "handlePanGesture:"))
+        self.layer.cornerRadius = 10
+        self.layer.shadowRadius = 4
+        self.layer.shadowOpacity = 0.6
+        self.layer.shadowOffset = CGSizeMake(1, 1)
+
+    }
     
     // MARK: Gestures Handling
     
     private struct Constants {
-        static let ActionMargin: CGFloat  = 200
+        static let SwipeDistanceToTakeAction: CGFloat  = 200
     }
     
-    var xFromCenter = CGFloat()
-    var yFromCenter = CGFloat()
+    var swipeDistanceOnY = CGFloat() //Distance of the swipe over "y" axis
     var originalPoint = CGPoint()
     
-    func handlePanGestureCell(sender: UIPanGestureRecognizer) {
+    func handlePanGesture(sender: UIPanGestureRecognizer) {
         
-        xFromCenter = sender.translationInView(self).x
-        yFromCenter = sender.translationInView(self).y
+        swipeDistanceOnY = sender.translationInView(self).y
         
         switch sender.state {
         case .Began:
             originalPoint = self.center
         case .Changed:
             println("Gesture Changed")
-            self.center = CGPointMake(originalPoint.x, originalPoint.y + yFromCenter)
-            println("yFromCenter: \(yFromCenter)")
+            self.center = CGPointMake(originalPoint.x, originalPoint.y + swipeDistanceOnY)
         case .Ended:
             println("Gesture Ended")
             afterSwipeAction()
@@ -66,46 +57,39 @@ class CardCollectionViewCell: UICollectionViewCell {
     }
     
     func afterSwipeAction() {
-        if yFromCenter > Constants.ActionMargin {
+        if swipeDistanceOnY > Constants.SwipeDistanceToTakeAction {
             println("Down")
             downAction()
-        } else if yFromCenter < -Constants.ActionMargin {
+        } else if swipeDistanceOnY < -Constants.SwipeDistanceToTakeAction {
             println("UP")
             upAction()
         } else {
             UIView.animateWithDuration(0.15, animations: {
-                println("CENTER: \(self.yFromCenter)")
-                
+                println("CENTER: \(self.swipeDistanceOnY)")
                self.center = self.originalPoint
-                
             })
         }
     }
     
     func upAction() {
-        let finishPoint: CGPoint = CGPointMake(originalPoint.x, -frame.maxY)
+        let maxTopPoint: CGPoint = CGPointMake(originalPoint.x, -frame.maxY)
         UIView.animateWithDuration(0.15, animations: { () -> Void in
-            self.center = finishPoint
+            self.center = maxTopPoint
         }) { (completion) -> Void in
             self.removeFromSuperview()
             self.setNeedsDisplay()
         }
-        
-        //        allTheCards.removeAtIndex(cellIndexPathToDelete.row)
-        //        collectionView?.setNeedsDisplay()
-        NSLog("YES")
     }
     
     func downAction() {
-        let finishPoint: CGPoint = CGPointMake(originalPoint.x, 2 * frame.maxY)
-        
-        UIView.animateWithDuration(0.15, animations: {
-            self.center = finishPoint
-        })
-        
-        NSLog("NO")
+        let maxDownPoint: CGPoint = CGPointMake(originalPoint.x, 2 * frame.maxY)
+        UIView.animateWithDuration(0.15, animations: { () -> Void in
+            self.center = maxDownPoint
+            }) { (completion) -> Void in
+                self.removeFromSuperview()
+                self.setNeedsDisplay()
+        }
     }
-
 
     
 }
--- Swiping Carousel/SwipingCarouselCollectionViewController.swift
+++ Swiping Carousel/SwipingCarouselCollectionViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 let reuseIdentifier = "Card"
 
-
-
-
 class SwipingCarouselCollectionViewController: UICollectionViewController {
     
     // MARK: Model
@@ -24,8 +21,7 @@ class SwipingCarouselCollectionViewController: UICollectionViewController {
     }
     // Create the Color Pallete using the UIColor Extension
     let colors = UIColor.palette()
-
-
+    
     // MARK: UICollectionViewDataSource
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -33,11 +29,9 @@ class SwipingCarouselCollectionViewController: UICollectionViewController {
         return allTheCards.count
     }
 
-
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CardCollectionViewCell
-        
         
         // Configure the cell
         let currentCard = allTheCards[indexPath.row]
@@ -47,87 +41,10 @@ class SwipingCarouselCollectionViewController: UICollectionViewController {
         cell.mainDescriptionLabel.text = currentCard.mainDescription
         cell.activityLabel.text = currentCard.activity
         cell.backgroundColor = colors[indexPath.item]
-        cell.layer.cornerRadius = 10;
-        cell.addGestureRecognizer(UIPanGestureRecognizer(target: cell, action: "handlePanGestureCell:"))
-//        cell.layer.shadowRadius = 4;
-//        cell.layer.shadowOpacity = 0.6;
-//        cell.layer.shadowOffset = CGSizeMake(3, 3);
         return cell
     }
      
-    // MARK: Gestures Handling
-    
-//    var xFromCenter = CGFloat()
-//    var yFromCenter = CGFloat()
-//    var originalPoint = CGPoint()
-//    var swipedCell = UICollectionViewCell()
-//    var cellIndexPathToDelete = NSIndexPath()
-//
-//    func handlePanGesture(sender: UIPanGestureRecognizer) {
-//        
-//        let swipeLocation = sender.locationInView(collectionView!)
-//        if let swipeIndexPath = collectionView?.indexPathForItemAtPoint(swipeLocation) {
-//            swipedCell = collectionView!.cellForItemAtIndexPath(swipeIndexPath)!
-//            swipedCell.multipleTouchEnabled = false
-//            cellIndexPathToDelete = swipeIndexPath
-//        } else {
-//            return
-//        }
-//        
-//        xFromCenter = sender.translationInView(swipedCell).x
-//        yFromCenter = sender.translationInView(swipedCell).y
-//      
-//        switch sender.state {
-//        case .Began:
-//            originalPoint = swipedCell.center
-//        case .Changed:
-//             println("Gesture Changed")
-//             swipedCell.center = CGPointMake(originalPoint.x, originalPoint.y + yFromCenter)
-//             println("yFromCenter: \(yFromCenter)")
-//        case .Ended:
-//            println("Gesture Ended")
-//            afterSwipeAction()
-//        default:
-//            break
-//        }
-//    }
-//    
-//    func afterSwipeAction() {
-//        if yFromCenter > ACTION_MARGIN {
-//            println("Down")
-//            downAction()
-//        } else if yFromCenter < -ACTION_MARGIN {
-//            println("UP")
-//            upAction()
-//        } else {
-//            UIView.animateWithDuration(0.15, animations: {
-//                println("CENTER: \(self.yFromCenter)")
-//                
-//                self.swipedCell.center = self.originalPoint
-//              
-//            })
-//        }
-//    }
-//    
-//    func upAction() {
-//        let finishPoint: CGPoint = CGPointMake(originalPoint.x, -self.view.frame.maxY)
-//        UIView.animateWithDuration(0.15, animations: {
-//            self.swipedCell.center = finishPoint
-//        })
-////        allTheCards.removeAtIndex(cellIndexPathToDelete.row)
-////        collectionView?.setNeedsDisplay()
-//        NSLog("YES")
-//    }
-//    
-//    func downAction() {
-//        let finishPoint: CGPoint = CGPointMake(originalPoint.x, 2 * self.view.frame.maxY)
-//        
-//        UIView.animateWithDuration(0.15, animations: {
-//            self.swipedCell.center = finishPoint
-//        })
-//    
-//        NSLog("NO")
-//    }
+
 
 }
     //Extension to create a pallete of colors which is being used to set the cell.backgroundColor
