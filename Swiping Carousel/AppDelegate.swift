d08fce1ebb8a853291021200e894d5d014bb934c
Pablo Paciello <ppacie@gmail.com>
2015-08-25 21:10:45 -0300

    Add some images and make the status bar white style.

--- Swiping Carousel/AppDelegate.swift
+++ Swiping Carousel/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         // Hide the status bar
-        UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: .None)
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
         return true
     }
 
