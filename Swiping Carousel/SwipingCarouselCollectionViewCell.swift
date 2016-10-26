0a4d8c952c0a3406c4db6930f28120236a23247d
Pablo Paciello <ppacie@gmail.com>
2016-10-26 21:14:55 +0200

    Update SwipingCarouselCollectionViewCell name.

--- Swiping Carousel/SwipingCarouselCollectionViewCell.swift
+++ Swiping Carousel/SwipingCarouselCollectionViewCell.swift
@@ -10,8 +10,8 @@ import UIKit
 
 //Protocol to inform its delegate of a Card being swiped up or down.
 protocol SwipingCarouselCellDelegate : class {
-    func cardSwipedUp(_ cell: SwipingCarouselCollectionViewCell)
-    func cardSwipedDown(_ cell: SwipingCarouselCollectionViewCell)
+    func cardSwipedUp(_ cell: UICollectionViewCell)
+    func cardSwipedDown(_ cell: UICollectionViewCell)
 }
 
 class SwipingCarouselCollectionViewCell: UICollectionViewCell {
@@ -20,14 +20,14 @@ class SwipingCarouselCollectionViewCell: UICollectionViewCell {
     
     required init(coder: NSCoder) {
         super.init(coder: coder)!
+        
         // Add Gesture to Cell
-        self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(CardCollectionViewCell.handlePanGesture(_:))))
+        self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:))))
     }
-
+ 
     // MARK: Gestures Handling
-    
     fileprivate struct Constants {
-        static let SwipeDistanceToTakeAction: CGFloat  = UIScreen.main.bounds.size.height / 5 //140 //Distance required for the card to go off the screen.
+        static let SwipeDistanceToTakeAction: CGFloat  = UIScreen.main.bounds.size.height / 5 //Distance required for the card to go off the screen.
         static let SwipeImageAnimationDuration: TimeInterval = 0.30 //Duration of the Animation when Swiping Up/Down.
         static let CenterImageAnimationDuration: TimeInterval = 0.20 //Duration of the Animation when image gets back to original postion.
     }
@@ -35,7 +35,7 @@ class SwipingCarouselCollectionViewCell: UICollectionViewCell {
     fileprivate var swipeDistanceOnY = CGFloat() //Distance of the swipe over "y" axis.
     fileprivate var originalPoint = CGPoint()
     
-    func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+    @objc fileprivate func handlePanGesture(_ sender: UIPanGestureRecognizer) {
         
         swipeDistanceOnY = sender.translation(in: self).y //Get the distance of the Swipe on "y" axis.
         
@@ -53,7 +53,7 @@ class SwipingCarouselCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    func afterSwipeAction() {
+    fileprivate func afterSwipeAction() {
         //First, we check if the swiped card is the one in the middle of screen by cheking its size. If the card is one of the sides, we send it back to its original position.
         if (self.frame.size.height > self.bounds.size.height) {
             //If the card is the one at the center (biggest one), we proceed to check wheather or not the distance of the gesture is enough to move the card off the screen (up or down).
@@ -75,7 +75,7 @@ class SwipingCarouselCollectionViewCell: UICollectionViewCell {
         
     }
     
-    func upAction() {
+   fileprivate func upAction() {
         UIView.animate(withDuration: Constants.SwipeImageAnimationDuration, animations: { () -> Void in
             self.center = self.originalPoint //Move the card back to the original point.
             self.superview?.isUserInteractionEnabled = false //Deactivate the user interaction in the Superview (In this case will be in the collection view). To avoid scrolling during the animation.
@@ -86,16 +86,14 @@ class SwipingCarouselCollectionViewCell: UICollectionViewCell {
         
     }
     
-    func downAction() {
+   fileprivate func downAction() {
         let maxDownPoint: CGPoint = CGPoint(x: originalPoint.x, y: 2 * frame.maxY)
         UIView.animate(withDuration: Constants.SwipeImageAnimationDuration, animations: { () -> Void in
             self.center = maxDownPoint //Move the card down off the screen.
             self.superview?.isUserInteractionEnabled = false //Deactivate the user interaction in the Superview (In this case will be in the collection view). To avoid scrolling during the animation.
             }, completion: { (completion) -> Void in
                 self.superview?.isUserInteractionEnabled = true // Re-activate the user interaction.
-                self.removeFromSuperview()
                 self.delegate?.cardSwipedDown(self) //Delegate the SwipeDown action and send the view with it.
         })
     }
-    
 }
