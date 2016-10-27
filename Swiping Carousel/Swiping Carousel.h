67de79e89580723525e96d92d175628dc9aeee8c
Pablo Paciello <ppacie@gmail.com>
2016-10-28 01:34:44 +0200

    Update structure

---
+++ Swiping Carousel/Swiping Carousel.h
@@ -0,0 +1,19 @@
+//
+//  Swiping Carousel.h
+//  Swiping Carousel
+//
+//  Created by Pablo Paciello on 10/27/16.
+//  Copyright © 2016 PPacie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiping Carousel.
+FOUNDATION_EXPORT double Swiping_CarouselVersionNumber;
+
+//! Project version string for Swiping Carousel.
+FOUNDATION_EXPORT const unsigned char Swiping_CarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiping_Carousel/PublicHeader.h>
+
+
