bb22744646865d392fa9fedf566de8065265d2e9
Pablo Paciello <ppacie@gmail.com>
2016-10-27 11:09:59 +0200

    Update file structure

---
+++ Example/Swiping Carousel/CardCollectionViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  CardCollectionViewCell.swift
+//  Swiping Carousel
+//
+//  Created by Pablo Paciello on 8/20/15.
+//  Copyright (c) 2015 Pablo Paciello. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: SwipingCarouselCollectionViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var professionLabel: UILabel!
+    @IBOutlet weak var mainDescriptionLabel: UILabel!
+    @IBOutlet weak var activityLabel: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    
+        // Cell Corner and Shadows
+        layer.cornerRadius = 10
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.6
+        // Emphasize the shadow on the bottom and right sides of the cell
+        layer.shadowOffset = CGSize(width: 4, height: 4)
+    }
+    
+    func populateWith(card: Card) {
+        
+        profileImage.image = card.image
+        nameLabel.text = card.name
+        professionLabel.text = card.profession
+        mainDescriptionLabel.text = card.mainDescription
+        activityLabel.text = card.activity
+        backgroundColor = card.backgroundColor        
+        likeImage.image = card.likedCard! ? UIImage(named: "Liked") : UIImage(named:"Disliked")
+    }
+}
---
+++ Example/Swiping CarouselTests/Swiping_CarouselTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swiping_CarouselTests.swift
+//  Swiping CarouselTests
+//
+//  Created by Pablo Paciello on 8/20/15.
+//  Copyright (c) 2015 Pablo Paciello. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Swiping_CarouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
